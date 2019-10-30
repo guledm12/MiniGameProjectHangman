589218bb6f5d7a931bb05e50863e9938f72bb0d9
S331474189 <S331474189@LDHS-BCT-131454.ad.ocdsb.ca>
2019-10-30 09:17:03 -0400

    very start

--- HangmanGameICS/src/hangmangameics/HangmanGameICS.java
+++ HangmanGameICS/src/hangmangameics/HangmanGameICS.java
@@ -13,7 +13,7 @@ public class HangmanGameICS {
     
     
     public static void main(String[] args) {
-        System.out.println("Welcome.")
+        System.out.println("Welcome.");
     }
     
 }
